acf102948a7096663a784aca09103e1f2ffc150c
Soldatov Aleksey <Fotaxis@mail.ru>
2025-07-28 21:01:07 +0500

    Scheduling metrics fix

--- synthetic-human-core-starter/src/main/java/org/example/synth/core/SyntheticHumanCoreAutoConfiguration.java
+++ synthetic-human-core-starter/src/main/java/org/example/synth/core/SyntheticHumanCoreAutoConfiguration.java
@@ -1,10 +1,10 @@
 package org.example.synth.core;
 
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@AutoConfiguration
+@Configuration
 @EnableScheduling
 @ComponentScan(basePackages = "org.example.synth.core")
 public class SyntheticHumanCoreAutoConfiguration {
